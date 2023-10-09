99e23dc33b8d243022053298d787ba65ed36118a
Shivangi <140998647+Shivangi2207@users.noreply.github.com>
2023-10-09 19:44:39 +0530

    Update shivangi_code.c

--- shivangi_code.c
+++ shivangi_code.c
@@ -1,79 +1,105 @@
-#include <stdio.h>
-//#include <wiringPi.h> // For Raspberry Pi GPIO control
-//#include <unistd.h>
+//#include <stdio.h>
+void Flame_detected();
+void  Flame_monitor();
 
-const int buzzerPin = 1; // GPIO pin for the buzzer (adjust for your setup)
-const int flamePin = 2;
-const int ledpin =3;// GPIO pin for the flame sensor (adjust for your setup)
-int Flame =0; // Assume HIGH (no flame) initially
-int buzzer =1;
-int led =1 ;
-int buzzer_reg ;
-int led_reg ;
 
-void delay(int milliseconds) {
-    for (int i = 0; i < milliseconds; i++) {
-        for (int j = 0; j < 10000; j++) {
-            // Adjust the loop count based on your microcontroller's speed
-        }
-    }
+int main (){
+	while(1){
+	Flame_detected();
+	}
+	return 0;
 }
 
+void Flame_monitor(){
+	int Flame;
+	int buzzer =0;
+	int buzzer_reg = buzzer *2;
+	
+	int led = 0;
+	int led_reg = led*4;
+	
+	
+        asm volatile(
+        "andi %0, x30, 1\n\t"
 
-
-void main() {
-  //  wiringPiSetupGpio(); // Initialize the wiringPi library
-
-//    pinMode(buzzerPin, OUTPUT);
-  //  pinMode(flamePin, INPUT);
- // pinMode(ledpin,OUTPUT); 
-  
-
-    while (1) {
+        :"=r"(Flame)
+        :
+        :
+        );
         
-        if (Flame == 0) { // LOW indicates fire detection
-          // printf("Fire is Detected\n");
-        //  Flame  = digital_read (0);
-           // digitalWrite(buzzerPin, HIGH);
-           //digitalWrite(ledpin, HIGH);
-           asm(
-        	"andi %0, x30, 1\n\t"
-        	:"=r"(Flame));
-         // buzzer = digitalwrite(1);
-         // printf("Buzzer is on,fire detected ");
-         buzzer =1;
-          	buzzer_reg = buzzer*2;
-        	asm(
-        	"or x30, x30,%0 \n\t"
-        	:"=r"(buzzer_reg));
-        
-          led = 1;
-          led_reg = led*4;
-          asm(
-        	"or x30, x30,%0 \n\t"
-        	:"=r"(led_reg));
+        asm volatile(
+	"or x30, x30, %0\n\t" 
+	:
+	:"r"(buzzer_reg)
+	:"x30"
+	);
+	
+	asm volatile(
+	"or x30, x30, %0\n\t" 
+	:
+	:"r"(led_reg)
+	:"x30"
+	);
+	
+	while(1){
+		if(Flame == 1){
+		// Flame = digital_read(0);
+		//buzzer = digitalwrite(1);
+		// led = digitalwrite(1);
+		//printf("Flame is detected");
+		buzzer = 1;
+		buzzer_reg=buzzer*2;
+		
+		led=1;
+		led_reg=led*4;
+		
+	  asm volatile(
+	"or x30, x30, %0\n\t" 
+	:
+	:"r"(buzzer_reg)
+	:"x30"
+	);
+          asm volatile(
+	  "or x30, x30,%0 \n\t"
+          :
+	  :"r"(led_reg)
+         :"x30"
+          );
+		
+		}
+		
+	else{
+	// Flame= digital_read(1);
+	// buzzer= digitalwrite(0);
+	//printf("Flame not detected");
+	buzzer = 0;
+		buzzer_reg=buzzer*2;
+		
+		led=0;
+		led_reg=led*4;
+		
+	  asm volatile(
+	"or x30, x30, %0\n\t" 
+	:
+	:"r"(buzzer_reg)
+	:"x30"
+	);
           
-        } 
-        else {
-          // printf("No Fire is Detected\n");
-         //  buzzer= digitalWrite(0);
-           buzzer =0;
-           buzzer_reg = buzzer*2;
-        	asm(
-        	"or x30, x30,%0 \n\t"
-        	:"=r"(buzzer_reg));
-        	
-       // led= digitalWrite(0);
-        
-          led = 0;
-          led_reg = led*4;
-          asm(
-        	"or x30, x30,%0 \n\t"
-        	:"=r"(led_reg));
-          
-        } 
-        }
-        
-        delay(1000); // Sleep for 100 milliseconds (adjust as needed)
-    }
+          asm volatile(
+	  "or x30, x30,%0 \n\t"
+          :
+	  :"r"(led_reg)
+          :"x30"
+          );
+	
+	
+		
+	}
+	}
 
+	
+}
+
+void Flame_detected(){
+	Flame_monitor();
+}
