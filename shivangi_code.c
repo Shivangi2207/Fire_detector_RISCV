ff6eb2f5e6dd610c41daf93a743c4484ebf878a8
Shivangi <140998647+Shivangi2207@users.noreply.github.com>
2023-10-21 21:14:28 +0530

    Update shivangi_code.c

--- shivangi_code.c
+++ shivangi_code.c
@@ -1,105 +1,74 @@
 //#include <stdio.h>
-void Flame_detected();
-void  Flame_monitor();
+
 
 
 int main (){
-	while(1){
-	Flame_detected();
-	}
-	return 0;
-}
-
-void Flame_monitor(){
-	int Flame;
+       
+        int Flame;
 	int buzzer =0;
 	int buzzer_reg = buzzer *2;
 	
 	int led = 0;
 	int led_reg = led*4;
+      
+	
+       asm volatile(
+	"or x30, x30, %0\n\t" 
+	"or x30, x30, %1\n\t" 
+	:
+	:"r"(buzzer_reg), "r"(led_reg)
+	:"x30"
+	);
 	
 	
-        asm volatile(
-        "andi %0, x30, 1\n\t"
+	
+	while(1){
+	
+	
+       asm volatile(
+       "andi %0, x30, 1\n\t"
 
         :"=r"(Flame)
         :
         :
         );
         
-        asm volatile(
-	"or x30, x30, %0\n\t" 
-	:
-	:"r"(buzzer_reg)
-	:"x30"
-	);
-	
-	asm volatile(
-	"or x30, x30, %0\n\t" 
-	:
-	:"r"(led_reg)
-	:"x30"
-	);
-	
-	while(1){
-		if(Flame == 1){
+		if(Flame == 1)
+		{
 		// Flame = digital_read(0);
 		//buzzer = digitalwrite(1);
 		// led = digitalwrite(1);
 		//printf("Flame is detected");
 		buzzer = 1;
-		buzzer_reg=buzzer*2;
-		
-		led=1;
-		led_reg=led*4;
-		
-	  asm volatile(
-	"or x30, x30, %0\n\t" 
-	:
-	:"r"(buzzer_reg)
-	:"x30"
-	);
-          asm volatile(
-	  "or x30, x30,%0 \n\t"
-          :
-	  :"r"(led_reg)
-         :"x30"
-          );
-		
+	        led=1;
 		}
 		
 	else{
+	
 	// Flame= digital_read(1);
 	// buzzer= digitalwrite(0);
 	//printf("Flame not detected");
-	buzzer = 0;
-		buzzer_reg=buzzer*2;
-		
+	        
+	        buzzer = 0;
 		led=0;
-		led_reg=led*4;
 		
-	  asm volatile(
+	}
+	
+	buzzer_reg = buzzer *2;
+	led_reg = led*4;
+    
+    asm volatile(
 	"or x30, x30, %0\n\t" 
+	"or x30, x30, %1\n\t" 
 	:
-	:"r"(buzzer_reg)
+	:"r"(buzzer_reg), "r"(led_reg)
 	:"x30"
 	);
-          
-          asm volatile(
-	  "or x30, x30,%0 \n\t"
-          :
-	  :"r"(led_reg)
-          :"x30"
-          );
 	
 	
-		
 	}
-	}
-
+	
+	return 0;
 	
 }
 
-void Flame_detected(){
-	Flame_monitor();
-}
